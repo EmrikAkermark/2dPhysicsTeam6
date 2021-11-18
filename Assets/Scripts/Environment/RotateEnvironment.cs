e35a88e24fdcccabe0427a0581bdc5a9f652a6f1
EmrikAkermark <headcrab.farmer@gmail.com>
2021-11-18 14:40:26 +0100

    MAde the world rotatior work like the video, can only rotate counter-clockwise currently

--- Assets/Scripts/Environment/RotateEnvironment.cs
+++ Assets/Scripts/Environment/RotateEnvironment.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+
 
 namespace Environment
 {
@@ -13,14 +15,46 @@ namespace Environment
         public Direction rotateDirection = Direction.CW;
         public float rotationSpeed = 10;
 
+		public float TimeBetweenRotations = 10f;
+
+		public float RotationStep = 45f;
+
+		private int amountOfTurns = 0;
+		private float timer;
+
         private void Start()
         {
             if (rotateDirection == Direction.CCW) rotationSpeed = rotationSpeed * -1;
         }
-        
-        private void Update()
-        {
-            transform.Rotate( Vector3.forward * ( -rotationSpeed * Time.deltaTime));
-        }
-    }
+
+		private void Update()
+		{
+			timer += Time.deltaTime;
+			if(timer >= TimeBetweenRotations)
+			{
+				timer = 0f;
+				StartCoroutine(RotateRoom());
+			}
+		}
+
+		private IEnumerator RotateRoom()
+		{
+			++amountOfTurns;
+			float newRotation = RotationStep * amountOfTurns;
+
+			bool finishedRotating = false;
+			while (!finishedRotating)
+			{
+				Debug.Log($"{transform.eulerAngles.z}, {newRotation}");
+				transform.Rotate(Vector3.forward * (-rotationSpeed * Time.deltaTime));
+				if(transform.eulerAngles.z >= newRotation)
+				{
+					Debug.Log($"{transform.eulerAngles.z}, {newRotation}");
+					transform.eulerAngles = new Vector3 {z = newRotation };
+					finishedRotating = true;
+				}
+				yield return null;
+			}
+		}
+	}
 }
